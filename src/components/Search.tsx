558203cf4ad87a25a20a24868ef87e198547345c
tejas-gk <tejgankri@outlook.com>
2023-03-03 09:15:29 +0530

    omkar said looks good

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -23,24 +23,25 @@ export const SearchContext = createContext<SearchContextProps>({
 
 export default function Search() {
     return (
-        <div className='search-container grid bg-card-shadow gap-10 rounded-md  p-10
+        <div className='search-container grid bg-card-shadow gap-10 rounded-md  p-10 md:w-full  w-[350px] 
       '>
             <form>
-                <div className="flex md:justify-between md:items-center rounded-md md:gap-10 bg-white p-5 shadow-lg shadow-card-shadow-dark
-              ">
+                <div className="flex justify-between items-center rounded-md md:gap-10 bg-white md:p-5  shadow-lg shadow-card-shadow-dark
+                w-[300px] md:w-full
+             ">
                     <div className="flex md:gap-2 items-center">
-                        <AiOutlineSearch className='text-[30px] font-bold cursor-pointer
+                        <AiOutlineSearch className='md:text-[30px] font-bold cursor-pointer text-[20px] ml-2 md:ml-0
                         '/>
                     </div>
                     <input type="text" className='md:w-full bg-transparent border-none focus:outline-none
-                    md:placeholder-gray-300 placeholder-gray-300  debug w-[250px]
+                    md:placeholder-gray-300 placeholder-gray-300  w-[250px] 
                     ' placeholder='Search for jobs, companies, etc.' />
 
-                    <AiOutlineCloseCircle className='text-[30px] font-bold cursor-pointer
+                    <AiOutlineCloseCircle className='md:text-[30px] font-bold cursor-pointer text-[20px]
                     '/>
                 </div>
                 <button className='bg-[#0a66c2] text-white rounded-md p-3 mt-3 md:mt-5 md:w-full hover:bg-[#0063e5]
-                w-[250px]
+                w-[300px]
                 '>Search</button>
             </form>
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -80,7 +80,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='md:w-[85%] m-auto 
+      <main className='md:w-[85%] m-auto w-full
       '>
         <Navbar />
         <Search />
